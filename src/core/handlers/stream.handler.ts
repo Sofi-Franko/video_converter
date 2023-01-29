5e6f8f9fbf7dc06ad25755a1a50a47301ab3f5e9
Sofiia Franchuk <sofiia.franchuk@plus4u.net>
2023-01-29 17:30:19 +0200

    handling output logic added

---
+++ src/core/handlers/stream.handler.ts
@@ -0,0 +1,18 @@
+import {IStreamLogger} from "./stream.logger.interface";
+import {ChildProcessWithoutNullStreams} from "child_process";
+
+export class StreamHandler {
+  constructor(private logger: IStreamLogger) {}
+
+  processOutput(stream: ChildProcessWithoutNullStreams) {
+    stream.stdout.on("data", (data: any) => {
+      this.logger.log(data)
+    })
+    stream.stderr.on("data", (data: any) => {
+      this.logger.error(data)
+    })
+    stream.on("close", () => {
+      this.logger.end()
+    })
+  }
+}
